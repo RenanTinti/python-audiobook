66f34181ff1d6391993b182bd6a48d0577026758
renantinti <55291956+RenanTinti@users.noreply.github.com>
2022-11-05 20:43:06 -0300

    set voice to english

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 import pyttsx3
+import PyPDF2
+
+book = open('python-audiobook/Book.pdf', 'rb')
+reader = PyPDF2.PdfFileReader(book)
+
+pages = reader.numPages
+print(pages)
 
 engine = pyttsx3.init()
 
-# rate = engine.getProperty('rate')
-engine.setProperty('rate', 90)
-engine.say("right? write?")
+for voice in engine.getProperty('voices'):
+    print(voice)
+
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[2].id)
+
+# Used to read a specific page
+page = reader.getPage(0)
+text = page.extractText()
+
+engine.setProperty('rate', 200)
+engine.say(text)
 
 engine.runAndWait()
